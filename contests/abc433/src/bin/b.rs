3f8687b2182a32dcae5917ffc0a3b26cc57db526
Kino-0 <86842153+Kino-0@users.noreply.github.com>
2025-11-23 13:23:13 +0900

    ✅ [AC] ABC433B - Nearest Taller

--- contests/abc433/src/bin/b.rs
+++ contests/abc433/src/bin/b.rs
@@ -1,2 +1,24 @@
 // B - Nearest Taller
 // https://atcoder.jp/contests/abc433/tasks/abc433_b
+
+use std::io;
+
+fn main() {
+    let input = io::read_to_string(io::stdin()).expect("標準入力読込失敗");
+    let tokens = input
+        .split_whitespace()
+        .map(|x| x.parse::<usize>().expect("パース失敗"));
+    let a: Vec<usize> = tokens.collect();
+    let n = a[0];
+
+    for i in 1..=n {
+        for j in (1..=i).rev() {
+            if a[i] < a[j] {
+                println!("{j}");
+                break;
+            } else if j == 1 {
+                println!("-1");
+            }
+        }
+    }
+}
